4a622c2ea942c791b3427609589289f367480db7
Johannes Terblanche <terblanche.j@gmail.com>
2017-12-11 09:53:23 +0200

    Try this mapping

--- examples/ps3eye.jl
+++ examples/ps3eye.jl
@@ -12,11 +12,15 @@ function displaylive!(im1, vidchan)
 end
 
 # This colour mapping may be wrong for the PS3eye.
-ycrcb = Video4Linux.UYVY(640,480)
-vidchan = Channel((c::Channel) -> videoproducer(c, ycrcb, devicename = "/dev/video1", iomethod = Video4Linux.IO_METHOD_MMAP ))
+ycrcb = Video4Linux.YUYV(640,480)
+vidchan = Channel((c::Channel) -> videoproducer(c, ycrcb, devicename = "/dev/video0", iomethod = Video4Linux.IO_METHOD_MMAP ))
 
 ##
 #capture one frame to create im1 and canvas needed to diplay
 A =  take!(vidchan)
 im1 = RGB.(YCbCr.(view(A,:,:,1), view(A,:,:,2), view(A,:,:,3)))
 canvas = imshow(im1)
+
+while isopen(vidchan)
+    displaylive!(im1, vidchan)
+end
